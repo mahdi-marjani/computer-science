8c8857208f111955d7dac0985ab4b4f6d091e3e7
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-12-06 10:57:58 +0330

    add main class

--- semester-3/fact.java
+++ semester-3/fact.java
@@ -1,12 +1,14 @@
-public static int factorial(int n) {
-    if (n == 0)
-        return 1;
-    else
-        return n * factorial(n - 1);
-}
+public class fact {
+    public static int factorial(int n) {
+        if (n == 0)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
 
-public static void main(String[] args) {
-    int num = 3;
-    int fact = factorial(num);
-    System.out.println("Factorial of " + num + " is: " + fact);
+    public static void main(String[] args) {
+        int num = 3;
+        int fact = factorial(num);
+        System.out.println("Factorial of " + num + " is: " + fact);
+    }
 }
--- semester-3/gcd.java
+++ semester-3/gcd.java
@@ -1,13 +1,15 @@
-public static int gcdFunc(int p, int q) {
-    if (q == 0)
-        return p;
-    else
-        return gcdFunc(q, p % q);
-}
+public class gcd {
+    public static int gcdFunc(int p, int q) {
+        if (q == 0)
+            return p;
+        else
+            return gcdFunc(q, p % q);
+    }
 
-public static void main(String[] args) {
-    int num1 = 10;
-    int num2 = 15;
-    int fact = gcdFunc(num1, num2);
-    System.out.println("gcd of " + num1 + ", " + num2 + " is: " + fact);
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 15;
+        int fact = gcdFunc(num1, num2);
+        System.out.println("gcd of " + num1 + ", " + num2 + " is: " + fact);
+    }
 }
